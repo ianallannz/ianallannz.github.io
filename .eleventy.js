7ee61fecddaf1c00c769da978a76422250aebbe6
Ian Allan <ian.allan.nz@gmail.com>
2025-11-04 07:00:28 +1300

    list actual posts

--- .eleventy.js
+++ .eleventy.js
@@ -1,33 +1,51 @@
-module.exports = function(eleventyConfig) {
-    const markdownIt = require("markdown-it");
-    const markdownItAnchor = require("markdown-it-anchor");
 
-    const md = markdownIt({ html: true, linkify: true })
-      .use(markdownItAnchor, { permalink: true, permalinkClass: "direct-link", permalinkSymbol: "#" });
 
-    // make a Nunjucks filter to render Markdown
-    eleventyConfig.addNunjucksFilter("markdown", function(value) {
-      return md.render(String(value || ""));
+module.exports = function (eleventyConfig) {
+  const markdownIt = require("markdown-it");
+  const markdownItAnchor = require("markdown-it-anchor");
+  const { DateTime } = require("luxon");
+
+  const md = markdownIt({ html: true, linkify: true })
+    .use(markdownItAnchor, { permalink: true, permalinkClass: "direct-link", permalinkSymbol: "#" });
+
+  // make a Nunjucks filter to render Markdown
+  eleventyConfig.addNunjucksFilter("markdown", function (value) {
+    return md.render(String(value || ""));
+  });
+
+  eleventyConfig.setLibrary("md", md);
+
+  // Set fixed blog number counts to posts
+  eleventyConfig.addCollection("blogWithNumbers", function (collectionApi) {
+    return collectionApi.getFilteredByTag("blog").map((item, index) => {
+      item.data.postNumber = index + 1;
+      return item;
+    });
+  });
+
+  // Date filter
+
+    eleventyConfig.addFilter("date", (dateObj, format = "dd LLLL yyyy") => {
+      return DateTime.fromJSDate(dateObj).toFormat(format);
     });
 
-    eleventyConfig.setLibrary("md", md);
 
-    // Copy static assets
-    eleventyConfig.addPassthroughCopy({"src/images": "images"});
-    eleventyConfig.addPassthroughCopy({"src/css": "css"});
-    eleventyConfig.addPassthroughCopy({"src/js": "js"});
+  // Copy static assets
+  eleventyConfig.addPassthroughCopy({ "src/images": "images" });
+  eleventyConfig.addPassthroughCopy({ "src/css": "css" });
+  eleventyConfig.addPassthroughCopy({ "src/js": "js" });
 
-    // Default layout settings
-    return {
-        dir: {
-            input: "src",
-            output: "_site",
-            includes: "_includes",
-            layouts: "_includes/layouts"
-        },
-        templateFormats: ["md", "njk", "html"],
-        markdownTemplateEngine: "njk",
-        htmlTemplateEngine: "njk",
-        dataTemplateEngine: "njk"
-    };
+  // Default layout settings
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      layouts: "_includes/layouts"
+    },
+    templateFormats: ["md", "njk", "html"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk"
+  };
 };
---
+++ src/blog/briefly-on-climate-change.md
@@ -0,0 +1,27 @@
+---
+title: Briefly on climate change, and retail
+date: 2025-09-26
+excerpt: Our thoughts on climate change were captured when we recorded our GDC candidate videos.
+layout: blog-post.njk
+tags: blog
+isPost: true
+---
+
+## Pursuing place, not things
+
+Thank you to the <a href="https://www.youtube.com/@ExchangeCafe" target="_blank">**Exchange Cafe**</a>&#8599; and <a href="https://www.rangai.nz/" target="_blank">Rāngai</a>&#8599; for collecting <a href="https://www.youtube.com/@ExchangeCafe/videos" target="_blank">candidate thoughts on climate change</a>&#8599; at the time of our council candidate videos.  
+
+<div class="video-container">
+    <iframe width="100%" src="https://www.youtube.com/embed/XYOfTlcCX7s?si=Qi-IPvLjVltTNDXh" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+</div>
+
+&nbsp;
+
+I do bang on about place, not things, and **that might sound disconcerting for local retailers**... but I love *local* retail. 
+
+This latest, beautiful example is what I believe the <a href="https://www.nzherald.co.nz/gisborne-herald/news/new-gisborne-creative-hub-offers-fabrics-fashion-workshops-and-local-design/EBBP3WKHONHJBONHEYYMHDO27M/" target="_blank">future of retail+value-added service</a>&#8599; looks like in a provincial resilient, sustainable, resourceful future place:
+
+<a href="https://www.nzherald.co.nz/gisborne-herald/news/new-gisborne-creative-hub-offers-fabrics-fashion-workshops-and-local-design/EBBP3WKHONHJBONHEYYMHDO27M/" target="_blank"><img src="../../images/gis-herald-snip-retail.jpg"></a>
+
+We can be the little resilient beacon of hope on the Coast. 
+
