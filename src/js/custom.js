83cf2dff98ba543aa87f299d538e7b954efc744a
Ian Allan <ian.allan.nz@gmail.com>
2025-11-12 22:23:08 +1300

    work js overlap tweak

--- src/js/custom.js
+++ src/js/custom.js
@@ -102,10 +102,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function isOverlapping(x1, y1, w1, h1, x2, y2, w2, h2) {
         return !(
-            x1 + w1 * 0.8 < x2 ||
-            x1 > x2 + w2 * 0.8 ||
-            y1 + h1 * 0.8 < y2 ||
-            y1 > y2 + h2 * 0.8
+            x1 + w1 * 0.9 < x2 ||
+            x1 > x2 + w2 * 0.9 ||
+            y1 + h1 * 0.9 < y2 ||
+            y1 > y2 + h2 * 0.9
         );
     }
 
