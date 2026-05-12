d8250ceb3ca2c872d2eb520f157ff0a4ecb8d126
ianallannz <ianallannz@users.noreply.github.com>
2026-05-12 18:35:18 +0000

    deploy: b42e748ac40b2dd7ee72b67083b72e65104ce24f

--- js/custom.js
+++ js/custom.js
@@ -96,6 +96,14 @@ const workImageData = [
     {
         image_url: '../images/work-occam-ui.jpg',
         related_how: 10
+    },
+    {
+        image_url: '../images/work-loadrite01.jpg',
+        related_how: 11
+    },
+    {
+        image_url: '../images/work-loadrite02.jpg',
+        related_how: 11
     }
 ];
 
@@ -110,7 +118,8 @@ const workHowData = [
     { id: 8, how_copy: "How do you propose a reimagined social content platform for the MOE after you failed with the product you led?" },
     { id: 9, how_copy: "How do you wrap reliable group assessment around experiential learning?" },
     { id: 10, how_copy: "How do you make a 20-year old software platform that's still bleeding-edge look bleeding-edge?" },
-    { id: 11, how_copy: "What question do you need answered?" }
+    { id: 11, how_copy: "How do you make a fun game on your company stand the talk of the trade show while collecting contacts?" },
+    { id: 12, how_copy: "What question do you need answered?" }
 ];
 
 
@@ -255,7 +264,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     renderAll();
-    setInterval(renderAll, 8000);
+    setInterval(renderAll, 10000);
 });
 
 
